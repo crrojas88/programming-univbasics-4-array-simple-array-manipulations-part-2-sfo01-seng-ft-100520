c5605e0098d044582f1ea0cdc0a0d0ec5b82ed1e
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-03 07:03:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,6 @@ def using_delete(array, string)
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots, 2)
+def using_delete_at(famous_robots, Integer)
   deleted_robot = famous_robots.delete_at(2)
 end
